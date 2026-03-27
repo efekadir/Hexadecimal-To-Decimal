695cc130622ecf1c7946515235ba26163bfbdfa4
Efe Kadir Küçük <efekadirkucuk@gmail.com>
2026-03-27 22:55:16 +0300

    Refactor HexToDecimal to use String validation and ASCII math

--- HexadecimalToDecimal.java
+++ HexadecimalToDecimal.java
@@ -3,24 +3,27 @@ import java.util.Scanner;
 public class HexadecimalToDecimal {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter one digit value: ");
+		String hex = sc.nextLine();
 		
-		System.out.print("Enter a hexadecimal value: ");
-		String hex = sc.next();
-		
-		if (hex.length() != 1) {
-			System.out.println("Please enter only one character.");
-			System.exit(0);
-		}
-		char ch = hex.toUpperCase().charAt(0);
-		if ('A' <= ch && ch <= 'F') {
-			int value = ch - 'A' + 10;
-			System.out.println("The decimal value for hexadecimal digit " + ch + " is " + value);
-		}
-		else if (Character.isDigit(ch)) {
-			System.out.println("The decimal value for hexadecimal digit " + ch + " is " + ch);
+		if (hex.length() == 1) {
+			hex = hex.toUpperCase();
+			char ch = hex.charAt(0);
+
+			if ('A' <= ch && ch <= 'F') {
+				int answer = ch - 'A' + 10;
+				System.out.println("Decimal value of " + hex + " is " + answer + ".");
+			}
+			else if ('0' <= ch && ch <= '9') {
+				int answer = ch - '0';
+				System.out.println("Decimal value of " + hex + " is " + answer + ".");
+			}
+			else {
+				System.out.println("Enter a valid value.");
+			}
 		}
 		else {
-			System.out.println("Please enter a one digit number or one of these letters: A, B, C, D, E, F");
+			System.out.println("Enter only one character.");
 		}
 	}
 }
